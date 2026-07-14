2252309d0a17d526d491009d55d04dd41b4a86cc
kavyadwm <kavyadwm987@gmail.com>
2026-07-15 00:17:18 +0530

    update

--- includes/footer.php
+++ includes/footer.php
@@ -4,3 +4,16 @@
 <script src="<?php echo $DOC_ROOT; ?>assets/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo $DOC_ROOT; ?>assets/js/swiper-bundle.min.js">
 </script>
+
+
+<script>
+    window.addEventListener("scroll", function() {
+        const header = document.querySelector(".sticky-header");
+
+        if (window.scrollY > 100) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    });
+</script>
